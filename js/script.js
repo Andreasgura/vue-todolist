6e99dbf7fc67e89d668989eec4cd7dc24cfe25c2
Andreasgura <andre-56@hotmail.it>
2024-04-02 17:04:19 +0200

    milestone 3

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ createApp({
     data() {
       return {
         toDo,
+        text : ''
       }
     },
     methods : {
@@ -17,6 +18,23 @@ createApp({
       removeItem(id){
         const i = this.toDo.findIndex((el) => el.id === id)
         this.toDo.splice(i,1)
+      },
+      addNewLi(text){
+        let newId = 0
+        this.toDo.forEach(el => {
+          if (el.id > newId) {
+            newId = el.id
+          }
+          newId = newId + 1;
+        });
+        let newLi = {
+          id: newId,
+          text,
+          done : false        
+      };
+      console.log(newId);
+      this.toDo.push(newLi);
+      this.text = ''       
       },
     },
     mounted () { 
