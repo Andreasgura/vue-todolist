5a8b448cc0c62cbe6f38f57aeba0cb6c0ba2a56e
Andreasgura <andre-56@hotmail.it>
2024-04-02 16:26:56 +0200

    milestone 2, bonus 2

--- js/script.js
+++ js/script.js
@@ -8,7 +8,16 @@ createApp({
       }
     },
     methods : {
-       
+      toggleDone(id){
+        const item = this.toDo.find((el) => el.id === id);
+        if (item){
+          item.done = !item.done;
+        }
+      },
+      removeItem(id){
+        const i = this.toDo.findIndex((el) => el.id === id)
+        this.toDo.splice(i,1)
+      },
     },
     mounted () { 
                      
